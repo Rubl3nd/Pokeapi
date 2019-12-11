8ad5da371a448610a5f1a3fe15b57102649e0ddb
unknown <rubl3nd@gmail.com>
2019-12-11 01:09:39 -0600

    Pokeapi comments

--- app/Http/Controllers/Pokehome.php
+++ app/Http/Controllers/Pokehome.php
@@ -5,24 +5,29 @@ use App\Repositories\Posts;
 use Illuminate\Http\Request;
 class Pokehome extends Controller
 {
+	//inyectamos la claase Posts por el constructor
 	protected $posts;
-
 	public function __construct(Posts $posts)
 	{
 		$this->posts=$posts;
 	}
+	//usamos la instancia de posts para pedir los datos
 	public function index()
 	{
 		$posts=$this->posts->all();
+		//una vez teniendo nuestra lista se lo mandamos a la vista home 
 		return view('home',compact('posts'));
 	}
 	public function name($id)
 	{
 		$name=$this->posts->byname($id);
+		//teniendo los datos de cada pokemon se los mandamos a la vista specific
 		return view('specific',compact('name','id'));
 	}
 	public function select(Request $request)
 	{
+		//guardamos todos los pokemon seleccionados para mostrarlos en otra vista
+		//esto se puede cambiar dependiedo si se desea mostrar en la misma vista
 		$guardados=implode(',',$request->input('seleccion'));
 		$info=explode(',',$guardados);
 		return view('seleccionados',compact('info'));
--- app/Repositories/Posts.php
+++ app/Repositories/Posts.php
@@ -1,20 +1,25 @@
 <?php
 
 namespace App\Repositories;
+//importamos la libreia que nos sugiere pokeapi
 use PokePHP\PokeApi;
 class Posts
 {
+	//constructor para la instanciacion del api
 	public function __construct()
 	{
 		$this->api=new PokeApi;
 	}
+	//Metodos que regresan datos de la pokeapi
 	public function all()
 	{
+		//obtenemos un listado con los pokemon y los retornamos 
 		$response = $this->api->resourceList('pokemon', '150', '11');
 		return json_decode($response);
 	}
 	public function byname($id)
 	{
+		//obtenemos los atributos de cada pokemon y los retornamos
 		$response = $this->api->pokemon($id);
 		return json_decode($response);
 	}
